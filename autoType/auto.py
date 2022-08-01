3b831756d248a4278dfcd0cdce00e4793ddcd21e
kedzkiest <kaede12kazu22@gmail.com>
2022-08-01 11:48:47 +0900

    added some margin before typing

--- autoType/auto.py
+++ autoType/auto.py
@@ -1,9 +1,11 @@
 import pyautogui as ag
+import time
 
 def main():
     f = open('autoType.txt', 'r', encoding='utf-8')
     text = f.read()
     f.close()
+    time.sleep(3)
     ag.typewrite(text, 0.001)
     
 if __name__ == '__main__':
